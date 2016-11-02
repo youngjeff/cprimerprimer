4ffc39dd818ec14ab1ef96622ab5c62e05e06f76
youngjeff <940107193@qq.com>
2016-11-02 18:47:40 +0800

    Create exceed.cpp

---
+++ 2/exceed.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#define ZERO 0
+#include <climits>
+
+int main()
+{
+	using namespace std;
+	short sam = SHRT_MAX;
+	unsigned short sue = sam;
+	
+	cout << "sam has" << sam << "dollars and Sue has " << sue;
+	cout << " dollars deposited." << endl
+		 << "Add $1 to each accout." << endl << "Now ";
+	sam = sam + 1;
+	sue = sue + 1;
+	cout << "Sam has " << sam << " dollars and Sue has " << sue;
+	cout << " dollars deposited.\n Poor Sam!" << endl;
+	sam = ZERO;
+	sue = ZERO;
+	cout << "Sam has " << sam << " dollars and Sue has " << sue;
+	cout << " dollars deposited." << endl;
+	cout << "Take $1 from each account." << endl << "Now ";
+	sam = sam - 1;
+	sue = sue - 1;
+	cout << "Sam has " << sam << " dollars and Sue has " << sue;
+	cout << " dollars deposited." << endl << "Lucky Sue!" << endl;
+	return 0;
+	
+}
